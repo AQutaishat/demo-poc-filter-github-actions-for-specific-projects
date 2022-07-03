aa2485f3a6f9e8c63edfc08264e863cf301f7d99
aqutaishat-cr2 <anas.qutaishat@cr2.com>
2022-07-03 23:54:52 +0300

    create two projects

--- project1/karma.conf.js
+++ project1/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/testGitWorkflow'),
+      dir: require('path').join(__dirname, './coverage/project1'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- project2/README.md
+++ project2/README.md
@@ -1,4 +1,4 @@
-# TestGitWorkflow
+# Project2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
 
